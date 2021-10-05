e2b7dd21d088c420d83e98cde3c4cc86f3af7e0d
lucachiaverini <35723079+lucachiaverini@users.noreply.github.com>
2021-10-05 12:59:03 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://98609d7f.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
